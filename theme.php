4f356058b8c40cfb147e6c013e9f663b599969e3
Fflam <Fflameq@gmail.com>
2024-09-28 03:21:23 -0700

    Update theme.php

--- theme.php
+++ theme.php
@@ -8,7 +8,7 @@ return [
     'author' => 'Pay2WinGaming',
 
     // Below are assets for the theme
-    'folder' => 'tailwind',
+    'folder' => 'pay2win',
 
     // Wrapper are used by addon developers to display the contents of a module
     // A wrapper should be a blank page with menu, user settings around it
