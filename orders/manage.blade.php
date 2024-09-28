2d57f3fc59913e282a56bd49035cf54a79ace08e
Fflam <Fflameq@gmail.com>
2024-09-28 04:10:03 -0700

    Update manage.blade.php

--- orders/manage.blade.php
+++ orders/manage.blade.php
@@ -101,20 +101,9 @@
                     <div class="mb-6">
                         <label for="password"
                             class="mb-2 block text-sm font-medium text-gray-900 dark:text-white">{{ __('auth.new_password') }}</label>
-                        <input type="password" name="password" id="hs-toggle-password"
+                        <input type="password" name="password" id="password"
                             class="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500"
                             placeholder="{{ __('auth.new_password') }}" required>
-                        <button type ="button" data-hs-toggle-password= '{"target": "#hs-toggle-password"}'
-                            class="absolute inset-y-0 end-0 flex items-center z-20 px-3 cursor-pointer text-gray-400 rounded-e-md focus:outline-none focus:text-blue-600 dark:text-neutral-600 dark:focus:text-blue-500">
-                            <svg class="shrink-0 size-3.5" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-                                <path class="hs-password-active:hidden" d="M9.88 9.88a3 3 0 1 0 4.24 4.24"></path>
-                                <path class="hs-password-active:hidden" d="M10.73 5.08A10.43 10.43 0 0 1 12 5c7 0 10 7 10 7a13.16 13.16 0 0 1-1.67 2.68"></path>
-                                <path class="hs-password-active:hidden" d="M6.61 6.61A13.526 13.526 0 0 0 2 12s3 7 10 7a9.74 9.74 0 0 0 5.39-1.61"></path>
-                                <line class="hs-password-active:hidden" x1="2" x2="22" y1="2" y2="22"></line>
-                                <path class="hidden hs-password-active:block" d="M2 12s3-7 10-7 10 7 10 7-3 7-10 7-10-7-10-7Z"></path>
-                                <circle class="hidden hs-password-active:block" cx="12" cy="12" r="3"></circle>
-                            </svg>
-                        </button>
                     </div>
 
                     <div class="mb-6">
